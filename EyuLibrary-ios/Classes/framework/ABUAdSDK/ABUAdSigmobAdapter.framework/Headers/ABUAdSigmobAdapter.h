337466c2005752d01cadd9caf20f21ec5f132ed5
eric <mrtanghaha@icloud.com>
2021-03-06 09:35:50 +0800

    commit

---
+++ EyuLibrary-ios/Classes/framework/ABUAdSDK/ABUAdSigmobAdapter.framework/Headers/ABUAdSigmobAdapter.h
@@ -0,0 +1,10 @@
+//
+//  ABUAdSigmobAdapter.h
+//  ABUAdSigmobAdapter
+//
+//  Created by XuQingJia on 2020/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ABUSigmobPersonaliseConfigAdapter.h"
---
+++ EyuLibrary-ios/Classes/framework/ABUAdSDK/ABUAdUnityAdapter.framework/Headers/ABUAdUnityAdapter.h
@@ -0,0 +1,11 @@
+//
+//  ABUAdUnityAdapter.h
+//  ABUAdUnityAdapter
+//
+//  Created by wangchao on 2020/5/29.
+//  Copyright © 2020 wangchao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ABUUnityPersonaliseConfigAdapter.h"
